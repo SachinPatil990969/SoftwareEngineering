be9e0420f19f4dd47dd42827b4652a8141b40c41
Sachin <Sachinpatil9737@gmail.com>
2023-05-10 11:57:42 +0530

    massage

---
+++ C Programming/Lecture Task/MyBio.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	printf("*************************************************\n");
+	printf("*	My name :- Sachin Patil			*\n");
+	printf("*	Mobile no :- 9737975710			*\n");
+	printf("*	Email :- sachinpatil9737@gmail.com	*\n");
+	printf("*	DOB :- 21/01/1998			*\n");
+	printf("*	Address :- 176,Madanpura,Surat		*\n");
+	printf("*	City :- Surat				*\n");
+	printf("*	Nationality :- Indian			*\n");
+	printf("*	Raligion :- Hindu			*\n");
+	printf("*	Education :- B.E.			*\n");
+	printf("*************************************************\n");
+}
