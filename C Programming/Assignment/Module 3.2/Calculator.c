fb7e2361a616d291cca7a32409a1eb95334f9d0e
Sachin <Sachinpatil9737@gmail.com>
2023-05-23 09:51:15 +0530

    New folder

---
+++ C Programming/Assignment/Module 3.2/Calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+	int a,b,c,d,e,g;
+	float f;
+	printf("\n Enter the value of a & b:");
+	scanf("%d %d",&a, &b);
+	c=a+b;
+	printf("\n Addition is %d",c);
+	d=a-b;
+	printf("\n Substraction is %d",d);
+	e=a*b;
+	printf("\n Multiplication is %d",e);
+	f=a/b;
+	printf("\n Division is %f",f);
+	g=a%b;
+	printf("\n Modulo is %d",g);
+}
---
+++ C Programming/Assignment/Module 3.2/odd even.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main(){
+	int x;
+	printf("\n Enter num:");
+	scanf("%d",&x);
+	(x%2==0)?printf("\n num is even"):printf("\n num is odd");
+}
